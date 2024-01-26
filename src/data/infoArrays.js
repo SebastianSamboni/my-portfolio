9877d16c598bfda5d7777f364d2fb0ce8c11f72c
SebastianSamboni <sebas_18_08@hotmail.com>
2024-01-26 15:12:21 -0500

    Create about-me structure

---
+++ src/data/infoArrays.js
@@ -0,0 +1,23 @@
+export const sections = [
+    {
+        id: 1,
+        dir: '/',
+        text_dir: 'Sobre Mí'
+    }, {
+        id: 2,
+        dir: '/my-stack',
+        text_dir: 'Mi Stack'
+    }, {
+        id: 3,
+        dir: '/projects',
+        text_dir: 'Proyectos'
+    }, {
+        id: 4,
+        dir: '/experience',
+        text_dir: 'Experiencia'
+    }, {
+        id: 5,
+        dir: '/contact',
+        text_dir: 'Contacto'
+    }
+]
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,16 +1,42 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import ProfileImg from '../assets/profile_img.jpeg'
 
 const AboutMe = () => {
+    const [currentText, setCurrentText] = useState(0)
+    const titles = [
+        'Desarrollador Web',
+        'Analista de Sistemas',
+        'Diseñador amateur'
+    ]
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentText((oldValue) => (oldValue + 1) % titles.length)
+        }, 3000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [titles.length])
+    
     return (
-        <div>
-            <Typography variant='h1'>
-                Juan Sebastián Samboni
+        <Grid container spacing={2}>
+            <Grid item>
+                <Typography variant='h3'>
+                    Juan Sebastián Samboni
+                </Typography>
+                <Typography variant='h4'>
+                    {titles[currentText]}
+                </Typography>
+            </Grid>
+            <Grid item>
+                <img src={ProfileImg} alt='profile_photo' style={{ width: '100px', height: '100px', borderRadius: '50%'}} />
+            </Grid>
+            <Typography variant='subtitle1'>
+                ¡Hola! Soy Juan Sebastián, desarrollador web y analista de 24 años apasionado por la creación de interfaces dinámicas y la robustez del back-end.
             </Typography>
-            <Typography variant='h3'>
-                Desarrollador Web
-            </Typography>
-        </div>
+        </Grid>
     )
 }
 
