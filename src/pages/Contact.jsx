c7bab6479e7b8a27a6b139c03f75a620a364ed45
SebastianSamboni <sebas_18_08@hotmail.com>
2024-01-29 09:56:10 -0500

    Create send info to mail method

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import {
+    Button,
     Container,
     Grid,
     TextField,
@@ -8,49 +9,81 @@ import {
 import React from 'react'
 
 const Contact = () => {
+    const clearForm = () => {
+        document.getElementById('name').value = ''
+        document.getElementById('surname').value = ''
+        document.getElementById('email').value = ''
+        document.getElementById('phone').value = ''
+        document.getElementById('description').value = ''
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        clearForm()
+    }
+
     return (
         <Container>
             <Typography variant='h4' textAlign='center'>
                 Ponte en Contacto!
             </Typography>
-            <form>
+            <form method='POST'
+                action='https://formspree.io/f/xayrqkbr'
+                onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
                     <Grid item md={6}>
                         <TextField id='name'
+                            name='name'
                             label='Nombre'
                             variant='outlined' 
                             required
+                            style={{ width: '100%'}}
                         />
                     </Grid>
                     <Grid item md={6}>
                         <TextField id='surname'
+                            name='surname'
                             label='Apellidos'
                             variant='outlined'
                             required
+                            style={{ width: '100%'}}
                         />
                     </Grid>
                     <Grid item md={6}>
                         <TextField id='email'
+                            name='email'
                             label='Correo'
                             variant='outlined'
                             required
+                            style={{ width: '100%'}}
                         />
                     </Grid>
                     <Grid item md={6}>
                         <TextField id='phone'
+                            name='phone'
                             label='Celular/Teléfono'
                             variant='outlined'
                             required
+                            style={{ width: '100%'}}
                         />
                     </Grid>
                     <Grid item md={12}>
                         <TextareaAutosize
                             minRows={3}
+                            name='description'
                             maxRows={10}
                             placeholder='Escribe tu pregunta!'
-                            style={{ resize: 'none', height:''}}
+                            style={{ resize: 'none', width: '100%', height: '150px'}}
                             required
                         />
+                    </Grid>
+                    <Grid item md={12}>
+                        <Button type='submit'
+                            variant='contained'
+                            fullWidth
+                        >
+                            Enviar
+                        </Button>
                     </Grid>
                 </Grid>
             </form>
